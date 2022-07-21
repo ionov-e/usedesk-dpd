ab41b5baa2c47de2eb88362f60cdef5b2f94e7f4
ionov-e <inv.great@gmail.com>
2022-07-22 00:38:48 +0300

    Нерабочая копипаста решения от DPD

---
+++ html/dpd-ready/city.php
@@ -0,0 +1,36 @@
+<?php
+function findCity($idcity) { //делаем функцию по поиску ключа города в DPD передавая город. (Пример - Калуга)
+    include "setings.php";
+    $client = new SoapClient ("$server[0]geography2?wsdl");
+
+    $arData['auth'] = array(
+        'clientNumber' => $MY_NUMBER,
+        'clientKey' => $MY_KEY);
+    $arRequest['request'] = $arData; //помещаем наш масив авторизации в масив запроса request.
+    $ret = $client->getCitiesCashPay($arRequest); //обращаемся к функции getCitiesCashPay  и получаем список городов.
+
+    function stdToArray($obj){
+        $rc = (array)$obj;
+        foreach($rc as $key=>$item){
+            $rc[$key]= (array)$item;
+            foreach($rc[$key] as $keys=>$items){
+                $rc[$key][$keys]= (array)$items;
+            }
+        }
+        return $rc;
+    }
+//функция отвечает за преобразования объекта в масив
+
+
+    $mass = stdToArray($ret); //вызываем эту самую функцию для того чтобы можно было перебрать масив
+
+    foreach($mass AS $key => $key1){
+        foreach($key1 AS $cityid => $city){
+            if (in_array($idcity,$city)) {$id = $city['cityId']; return $id;}// если мы находим этот город в масиве (который мы искали) мы заносим его в переменную $ID и возвращаем наш ответ.
+        }
+    }
+}
+
+//Пример запроса
+//$city = ‘Калуга’;
+//$findcity = findCity($city); //так мы запишем номер города из DPD в нашу переменную.
---
+++ html/dpd-ready/setting.php
@@ -0,0 +1,7 @@
+<?php
+$MY_NUMBER 	= 'учётная запись';
+$MY_KEY		= 'ключ учётной запиcи';
+$server = array(
+    0=>'http://ws.dpd.ru/services/', //обычный сервер
+    1=>'http://wstest.dpd.ru/services/' //тестовый сервер
+);
