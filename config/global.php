a4e42540c91369af3758d47b5485c8f616ed99ab
ionov-e <IxwSPnv87026>
2022-08-04 12:07:24 +0300

    Использование константы в ссылке для удобства

--- config/global.php
+++ config/global.php
@@ -12,7 +12,7 @@ try {
 }
 define('CLIENT_NUMBER', $_ENV['CLIENT_NUMBER']);
 define('CLIENT_KEY', $_ENV['CLIENT_KEY']);
-define('URL_SCRIPT_DOMAIN', $_ENV['URL_SCRIPT_DOMAIN']);
+define('URL_SCRIPT_ROOT', $_ENV['URL_SCRIPT_ROOT']);
 define('URL_DPD_DOMAIN', $_ENV['URL_DPD_DOMAIN']);
 define('FTP_SERVER', $_ENV['FTP_SERVER']);
 define('FTP_USER', $_ENV['FTP_USER']);
@@ -21,10 +21,11 @@ define('DPD_CITY_LIST_SAFE_MODE', $_ENV['DPD_CITY_LIST_SAFE_MODE']);
 
 define('PROJECT_DIR', dirname(__DIR__, 1));              // Корневая папка
 
-const URL_SCRIPT_PHP = URL_SCRIPT_DOMAIN . '/usedesk-dpd.php';      // URL на который должны приходить запросы от UseDesk
+const INDEX_FILE_USEDESK = 'usedesk-dpd.php';                       // Название файла куда будут приходить запросы от UseDesk
+const URL_SCRIPT_PHP = URL_SCRIPT_ROOT . DIRECTORY_SEPARATOR . INDEX_FILE_USEDESK; // URL путь к файлы выше
 const LOG_FOLDER_ROOT = PROJECT_DIR . DIRECTORY_SEPARATOR . 'log';  // Путь к папке для логов
 const DATA_FOLDER_ROOT = PROJECT_DIR . DIRECTORY_SEPARATOR . 'data';// Путь к папке для хранения данных
-const DATA_JSON = DATA_FOLDER_ROOT. DIRECTORY_SEPARATOR . 'bd.json';// Здесь наша главное БД (с ID тикетов, № ТТН, Статусом ТТН)
+const DATA_JSON = DATA_FOLDER_ROOT . DIRECTORY_SEPARATOR . 'bd.json';// Здесь наша главное БД (с ID тикетов, № ТТН, Статусом ТТН)
 const TTN_JSON_KEY = 'ttn';                                         // Под таким ключом хранится в JSON созданное ТТН для Тикета
 const STATE_JSON_KEY = 'state';                                     // Под таким ключом хранится в JSON статус созданного ТТН для Тикета
 const TICKET_ID_KEY_NAME = 'ticket_id';                             // Название параметра в Post-запросе от UseDesk
