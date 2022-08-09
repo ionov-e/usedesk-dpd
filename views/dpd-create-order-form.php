f0586b13debebe9e2fac1124ea2b5b460f40c4fd
ionov-e <IxwSPnv87026>
2022-08-09 14:28:09 +0300

    В форму создания ТТН изменил ссылку на css, чтобы стили подключались, если проект выложен на сервере не в корне папки

--- views/dpd-create-order-form.php
+++ views/dpd-create-order-form.php
@@ -19,7 +19,7 @@ $modifyDays = 1;
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
                 integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
                 crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="stylesheet" href="<?= URL_SCRIPT_ROOT ?>/assets/css/style.css">
     </head>
     <body>
         <div class="container">
