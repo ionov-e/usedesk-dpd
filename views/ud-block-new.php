7c9c76fa4775d3047d95cf0c73297b38d608d9c3
ionov-e <IxwSPnv87026>
2022-08-17 11:21:26 +0300

    При нажатии "Оформить ТТН" - открывается ссылка в новой странице

--- views/ud-block-new.php
+++ views/ud-block-new.php
@@ -2,4 +2,4 @@
 /**
  * @var $args array Аргументы извне
  * */ ?>
-<a class='btn btn-green' href='<?= URL_SCRIPT_PHP ?>?<?= TICKET_ID_KEY_NAME ?>=<?= $args[TICKET_ID_KEY_NAME] ?>'>Оформить ТТН</a>
+<a class='btn btn-green' target=”_blank” href='<?= URL_SCRIPT_PHP ?>?<?= TICKET_ID_KEY_NAME ?>=<?= $args[TICKET_ID_KEY_NAME] ?>'>Оформить ТТН</a>
